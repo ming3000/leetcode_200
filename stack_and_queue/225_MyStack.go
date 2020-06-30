176ba733185216f74b372718efddf14647dd3613
xushouming <xushouming@bytedance.com>
2020-06-30 20:52:53 +0800

    start

---
+++ stack_and_queue/225_MyStack.go
@@ -0,0 +1,41 @@
+package stack_and_queue
+
+type MyStack struct {
+	queue []int
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyStack {
+	return MyStack{queue: make([]int, 0)}
+}
+
+/** Push element x onto stack. */
+func (this *MyStack) Push(x int) {
+	// new element enqueue
+	this.queue = append(this.queue, x)
+	length := len(this.queue)
+	// all element dequeue from head and enqueue to the tail.
+	// except the new element
+	for i := 0; i < length-1; i++ {
+		head := this.queue[0]
+		this.queue = this.queue[1:]
+		this.queue = append(this.queue, head)
+	} // for>
+}
+
+/** Removes the element on top of the stack and returns that element. */
+func (this *MyStack) Pop() int {
+	ret := this.queue[0]
+	this.queue = this.queue[1:]
+	return ret
+}
+
+/** Get the top element. */
+func (this *MyStack) Top() int {
+	return this.queue[0]
+}
+
+/** Returns whether the stack is empty. */
+func (this *MyStack) Empty() bool {
+	return len(this.queue) == 0
+}
